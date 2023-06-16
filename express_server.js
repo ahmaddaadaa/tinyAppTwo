06821aae7ed8798a6895636cb5f711ac1052f64a
ahmaddaadaa <ahmadd@uvic.ca>
2023-06-15 22:38:36 -0700

    updated

--- express_server.js
+++ express_server.js
@@ -3,9 +3,13 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.use(morgan('dev'));
-app.set("view engine", "ejs"); // set us a view engine for our EJS files 
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2,8);
+};
 
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs"); // set us a view engine for our EJS files 
 
 
 
@@ -22,9 +26,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
