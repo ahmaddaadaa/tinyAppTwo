717aebb69ff0330fe37aa2e2f9ebcccf6791028b
ahmaddaadaa <ahmadd@uvic.ca>
2023-06-18 13:31:16 -0700

    updated

--- express_server.js
+++ express_server.js
@@ -30,17 +30,6 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
-
 
 app.post("/urls", (req, res) => {
   const generatedKey = generateRandomString();
@@ -49,11 +38,40 @@ app.post("/urls", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/edit/:id", (req, res) => {
+  const id = req.params.id; // Retrieve the ID from the request parameters
+  const newURL = req.body.longURL; 
+
+  urlDatabase[id] = newURL;
+
+  const templateVars = { id: id, longURL: newURL};
+  res.render("urls_show", templateVars);
+});
+
+// app.get("/urls/edit/:id", (req, res) => {
+//   console.log(res)
+  
+//   res.send("ok");
+// });
+
+
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
