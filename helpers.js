26e7206b671174864dee0efae24b49b6355dbb91
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-16 16:17:25 -0700

    contains all the functios used in the code

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
+// gets the user given the email
 const getUserByEmail = function(email, database) {
-  // lookup magic...
   for(let key in database){
     if(database[key].email === email)
     return key;
@@ -8,6 +8,29 @@ const getUserByEmail = function(email, database) {
 };
 
 
+// function checks if an Id exists in the URLs Database
+const verifyIfIdExists = function(id, database) {
+  for (const key in database) {
+    if (key === id) {
+      return true;
+    }
+  }
+};
+
+const urlsForUser = function(id, database) {
+  let result = {};
+  for (let key in database) {
+    if (database[key].userID === id)
+      result[key] = database[key].longURL;
+
+  };
+  return result;
+};
+
+// function to generate String ID
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2, 8);
+};
 
 
 
@@ -15,5 +38,9 @@ const getUserByEmail = function(email, database) {
 
 
 
-
-module.exports = {getUserByEmail};
+module.exports = {
+  getUserByEmail,
+  verifyIfIdExists,
+  urlsForUser,
+  generateRandomString
+};
