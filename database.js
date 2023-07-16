624fcba38fe991c907b957e8498d9b2b26cb72bc
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-16 16:18:43 -0700

    contains all databases for the app, urlDatabase and usersDatabase

---
+++ database.js
@@ -0,0 +1,35 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+
+  },
+
+  shortId: {
+    longURL: "ahmaddaadaa.ca",
+    userID: "userRandomID",
+  }
+
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+module.exports = {
+  urlDatabase,
+  users
+}
